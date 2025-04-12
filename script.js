b03fabc1fd3da5c179d1e026fe080f391e0c5321
Whiteshadow2664 <sjemma819@gmail.com>
2025-04-12 11:58:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
-  sidebar.style.right = sidebar.style.right === "0px" ? "-250px" : "0px";
+  sidebar.classList.toggle("open");
 }
