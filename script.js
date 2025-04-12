4a6446a7b9ed176dd9c5f4cd151688d9e6939c2d
Whiteshadow2664 <sjemma819@gmail.com>
2025-04-12 11:15:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function showContact(type) {
   const contact = {
-    phone: "Call me at: +91-1234567890",
-    email: "Email me at: your.email@example.com",
-    linkedin: "Connect on LinkedIn: linkedin.com/in/yourprofile"
+    phone: "📞 +91-1234567890",
+    email: "✉️ your.email@example.com",
+    linkedin: "🔗 linkedin.com/in/yourprofile"
   };
   document.getElementById('contact-info').innerText = contact[type];
 }
