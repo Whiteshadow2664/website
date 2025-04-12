76fd8220530e57ce978a78764d94f24492129261
Whiteshadow2664 <sjemma819@gmail.com>
2025-04-12 11:20:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function showContact(type) {
-  const contact = {
-    phone: "📞 +91-1234567890",
-    email: "✉️ your.email@example.com",
-    linkedin: "🔗 linkedin.com/in/yourprofile"
-  };
-  document.getElementById('contact-info').innerText = contact[type];
+function toggleSidebar() {
+  const sidebar = document.getElementById('sidebar');
+  if (sidebar.style.right === '0px') {
+    sidebar.style.right = '-250px';
+  } else {
+    sidebar.style.right = '0px';
+  }
 }
